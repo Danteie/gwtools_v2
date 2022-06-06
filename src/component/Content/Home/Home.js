491a2fb2babe5d78e9ec37cb3f2dc41a81f9d51f
unknown <aleksandar.djuratovic2@gmail.com>
2022-06-06 21:11:56 +0200

    adding buttons on home page

--- src/component/Content/Home/Home.js
+++ src/component/Content/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import WpFile from '../../../Assets/White Papers - updated 06.06.2022.docx'
 
 class Home extends Component{
     constructor(props) {
@@ -15,6 +16,13 @@ class Home extends Component{
                     <h2>Viewable impresion ne zaboraviti kada se setuje web kampanaj u navizi</h2>
                     <h2>Thermo Fisher Scientific Carlsbad kampanje obavzeno exclude Rusiju i Ukrainu</h2>
                     <h2>Somalogic web ima posebna pravila za zemlje</h2>
+                    <div>
+                        <button className="file-button"><a href={WpFile} download="WhitePapers 06.06.2022.docx">WHITE PAPRE FILE</a></button>
+                        <button className="file-button"><a href="https://docs.google.com/spreadsheets/d/1Lg1srIdXK_n5XZPyf5ktK2Ev0TUtP1eRHp5cUb9NVcI/edit#gid=1933187957">TASKOVI</a></button>
+                        <button className="file-button"><a href="https://docs.google.com/spreadsheets/d/1CJqHUzYR5_1nCxiN58JCM6BuggJ6wWekS-3YnY63BG0/edit#gid=447324662">US BULLETIN SHEET</a></button>
+                        <button className="file-button"><a href="https://docs.google.com/spreadsheets/d/1Qh0VKZpAOgBDmukV3gnTYSWqx3TDp5dWE-tFfqv4GBM/edit#gid=447324662">EU BULLETIN SHEET</a></button>
+                    </div>
+                    
             </div> 
         )
               
