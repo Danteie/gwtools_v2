6e39bcbf9a17e116ffadd97b45c806a990308fa5
Danteie <aleksandar.djuratovic93@gmail.com>
2022-07-26 18:07:26 +0200

    wp update

--- src/component/Title/Title.js
+++ src/component/Title/Title.js
@@ -7,7 +7,7 @@ const Title = () => {
         <div className="title">
             <Link to="/"><h1 className="flex justify-center title">GW TOOLS</h1></Link>
              <div className="file-button-container">
-                        <a className="file-button"  rel="noreferrer"y href={WpFile} download="WhitePapers 07.15.2022.docx">WHITE PAPRE FILE<br/><p className="small">last update: 2022 07 15</p></a>
+                        <a className="file-button"  rel="noreferrer"y href={WpFile} download="WhitePapers 07.21.2022.docx">WHITE PAPRE FILE<br/><p className="small">last update: 2022 07 21</p></a>
                         <a className="file-button"  rel="noreferrer" href="https://docs.google.com/spreadsheets/d/1Lg1srIdXK_n5XZPyf5ktK2Ev0TUtP1eRHp5cUb9NVcI/edit#gid=1933187957" target="_blank">TASKOVI</a>
                         <a className="file-button"  rel="noreferrer" href="https://docs.google.com/spreadsheets/d/1CJqHUzYR5_1nCxiN58JCM6BuggJ6wWekS-3YnY63BG0/edit#gid=447324662" target="_blank">US BULLETIN SHEET</a>
                         <a className="file-button"  rel="noreferrer" href="https://docs.google.com/spreadsheets/d/1Qh0VKZpAOgBDmukV3gnTYSWqx3TDp5dWE-tFfqv4GBM/edit#gid=447324662" target="_blank">EU BULLETIN SHEET</a>
